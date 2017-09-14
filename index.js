1f63545cc3848ab61da6f7d45842fa23c62a7605
amcnulty <amcnulty88@swbell.net>
2017-09-14 10:39:15 -0700

    Session Secret
    Working with the session secret.

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ var secret = require('./keys/secret');
 
 var app = express();
 
-// view engine setup
+// View engine setup
 // this is where you can set up an engine to render jade or ejs
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
